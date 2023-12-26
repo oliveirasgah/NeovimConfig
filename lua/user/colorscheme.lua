93ae21805cd78a101fe31ab4efb3192361d3abb0
Gabriel Oliveira dos Santos <oliveirasgah@gmail.com>
2023-12-26 11:58:14 -0300

    Adding colorscheme

---
+++ lua/user/colorscheme.lua
@@ -0,0 +1,14 @@
+local colorscheme = "kanagawa-wave"
+--local colorscheme = "kanagawa-dragon"
+--local colorscheme = "kanagawa-lotus"
+
+local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
+
+if not status_ok then
+  return
+else
+  -- Remove background from Neovim
+  vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+  --vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" }) -- Remove background from loats
+end
+
